b44e296359d7ca61103680ae85b2e69430eb389c
banders <banders@globalnoc.iu.edu>
2026-04-21 16:15:27 -0400

    Moved admin_api to the outer package and core metranova to packages

--- src/metranova/__init__.py
+++
@@ -1,9 +0,0 @@
-from metranova.storage.base import CollectionField, CollectionType, ConsumerType
-from metranova.storage.clickhouse import Clickhouse
-
-__all__ = [
-    "CollectionField",
-    "CollectionType",
-    "ConsumerType",
-    "Clickhouse",
-]
