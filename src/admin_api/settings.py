e9c0a65a75cb8343bc84d3d29982e6b813ef2a25
Jonathan Stout <jonstout@globalnoc.iu.edu>
2026-09-09 14:32:12 -0400

    Use environment variable for kube_namespace

--- src/admin_api/settings.py
+++ src/admin_api/settings.py
@@ -1,13 +1,14 @@
 from functools import lru_cache
-
 from pydantic_settings import BaseSettings
+
+import os
 
 
 class Settings(BaseSettings):
     cors_regex: str = r"^(https://((?!-)[A-Za-z0-9-]{1,63}(?!<-)\.)+grnoc\.iu\.edu)$"
     root_path: str = ""
     db_url: str = ""
-    kube_namespace: str = "metranova-test"
+    kube_namespace: str = os.getenv("METRANOVA_KUBE_NAMESPACE", "metranova-test")
 
 
 @lru_cache()
